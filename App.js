5567d4b367eca63e92b1e6c1e40eb98c6589b867
조원석 <59154288+jws020501@users.noreply.github.com>
2020-07-30 15:07:38 +0900

    123
    123

--- App.js
+++ App.js
@@ -1,91 +1,62 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput, Alert } from 'react-native';
-import MapView from 'react-native-maps';
-import { v4 as uuidv4 } from 'uuid';
+import Expo from "expo";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+const io = require("socket.io-client");
 
-import Editbar from './Editbar';
-import Marking from './CustomMarker';
-import SetLocationinfo from './SetLocationinfo';
+// Replace this URL with your own, if you want to run the backend locally!
+const SocketEndpoint = "https://server0501.herokuapp.com/";
 
-export default class maps extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			title: '',
-			coordinate: {},
-			markers: {},
-		};
-		this._touchMap = this._touchMap.bind(this);
-	}
+export default class App extends React.Component {
+  state = {
+    isConnected: false,
+    data: null,
+  };
+  componentDidMount() {
+    const socket = io(SocketEndpoint, {
+      transports: ["websocket"],
+    });
 
-	_setTitle = (text) => {
-		this.setState({
-			title: text,
-		});
-	};
+    socket.on("connect", () => {
+      this.setState({ isConnected: true });
+    });
+    socket.on("connection", () => {
+      this.setState({ isConnected: true });
+    });
+    var name = "김만덕";
+    socket.emit("asd", name);
+  }
 
-	_touchMap(e) {
-		this.setState({
-			coordinate: e.nativeEvent.coordinate,
-		});
-		this.setState((prevState) => {
-			const ID = uuidv4();
-			const newMarker = {
-				[ID]: {
-					id: ID,
-					title: this.title,
-					coordinate: this.coordinate,
-				},
-			};
-			const newState = {
-				...prevState,
-				markers: {
-					...prevState.markers,
-					...newMarker,
-				},
-			};
-			return { ...newState };
-		});
-		Alert.alert('클릭');
-		console.log(this.state);
-	}
-
-	render() {
-		return (
-			<View style={{ flex: 1 }}>
-				<Editbar />
-				<MapView
-					style={styles.container}
-					initialRegion={{
-						latitude: 45.5209087,
-						longitude: -122.6705107,
-						latitudeDelta: 0.0922,
-						longitudeDelta: 0.0421,
-					}}
-					//onPress={this._touchMap}
-				></MapView>
-				<TextInput
-					style={styles.Input}
-					placeholder={'약속 이름'}
-					placeholderTextColor={'#999'}
-					onSubmitEditing={this._setTitle}
-				/>
-			</View>
-		);
-	}
+  render() {
+    return <View style={styles.container}></View>;
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 3,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	Input: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 170,
+    borderColor: "gray",
+  },
+  sendbtn: {
+    height: 40,
+    width: 45,
+    backgroundColor: "blue",
+  },
+  textbar: {
+    height: 45,
+  },
 });
